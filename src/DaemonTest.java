2f24061ec6e614d4c8f1226454fb31da2df9ee35
jsh940517 <jsh940517@daum.net>
2022-05-22 14:49:18 +0900

    성능저하 개선. 데몬 스레드로 스레드를 돌리는 것보다는 메인 스레드 하나로 돌리는 것이 성능이 더 좋다.

--- src/DaemonTest.java
+++ src/DaemonTest.java
@@ -8,9 +8,17 @@ import java.nio.file.Paths;
 
 import javax.swing.ImageIcon;
 
-public class DaemonTest extends Thread{
+/**
+ * @author 전상훈
+ * 
+ * 데몬스레드를 돌리기 위해서 작성했지만
+ * 
+ * 메인 스레드 하나로 돌릴때가 더 이용률을 낮춰준다.
+ *
+ */
+public class DaemonTest/* extends Thread*/{
 	
-	private File dir = new File("C:/dev/test/");
+//	private File dir = new File("C:/dev/test/");
 	
 	private static void deleteTxtFile(File file) {
 		file.delete();
@@ -25,58 +33,58 @@ public class DaemonTest extends Thread{
 		}
 	}
 	
-	@Override
-	public void run() {
-		// TODO Auto-generated method stub
-//		synchronized(this) {
-			
-			if(!dir.exists()) {
-				dir.mkdirs();
-			}
-			
-			if(Thread.currentThread().isDaemon()) {
-				//무한루프 돌리기
-				System.out.println("데몬이 실행중입니다");
-				while(true) {
-					File[] txtList = dir.listFiles(new FilenameFilter() {
-						
-						@Override
-						public boolean accept(File dir, String name) {
-							// TODO Auto-generated method stub
-							boolean isTxt = false;
-							
-							if(name.toLowerCase().startsWith("새") && name.toLowerCase().endsWith(".txt") == true) isTxt = true;
-							
-							return isTxt;
-						}
-					});
-					
-					if(txtList.length > 0) {
-						threadSleep(600);
-						try {
-							for(File txt : txtList) {
-								//파일이 존재합니다.
-								System.out.println("파일이 존재합니다.");
-								threadSleep(800);
-								deleteTxtFile(txt);
-								System.out.println("파일을 삭제했습니다.");
-								threadSleep(800);
-							}
-						} catch(Exception e) {
-							threadSleep(400);
-							return;
-						}
-					}
-					
-					threadSleep(10000);
-				}
-			} else {
-				return;
-			}
-			
-//		}
-		
-	}
+//	@Override
+//	public void run() {
+//		// TODO Auto-generated method stub
+////		synchronized(this) {
+//			
+//			if(!dir.exists()) {
+//				dir.mkdirs();
+//			}
+//			
+//			if(Thread.currentThread().isDaemon()) {
+//				//무한루프 돌리기
+//				System.out.println("데몬이 실행중입니다");
+//				while(true) {
+//					File[] txtList = dir.listFiles(new FilenameFilter() {
+//						
+//						@Override
+//						public boolean accept(File dir, String name) {
+//							// TODO Auto-generated method stub
+//							boolean isTxt = false;
+//							
+//							if(name.toLowerCase().startsWith("새") && name.toLowerCase().endsWith(".txt") == true) isTxt = true;
+//							
+//							return isTxt;
+//						}
+//					});
+//					
+//					if(txtList.length > 0) {
+//						threadSleep(600);
+//						try {
+//							for(File txt : txtList) {
+//								//파일이 존재합니다.
+//								System.out.println("파일이 존재합니다.");
+//								threadSleep(800);
+//								deleteTxtFile(txt);
+//								System.out.println("파일을 삭제했습니다.");
+//								threadSleep(800);
+//							}
+//						} catch(Exception e) {
+//							threadSleep(400);
+//							return;
+//						}
+//					}
+//					
+//					threadSleep(10000);
+//				}
+//			} else {
+//				return;
+//			}
+//			
+////		}
+//		
+//	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -103,11 +111,58 @@ public class DaemonTest extends Thread{
 		
 		TrayIconHandler.displayMessage("TestDaemon", "The program is runnung!", MessageType.INFO);
 		
-		DaemonTest daemon = new DaemonTest();
-		daemon.setDaemon(true);
-		daemon.start();
+//		DaemonTest daemon = new DaemonTest();
+//		daemon.setDaemon(true);
+//		daemon.start();
 		
-		while(true) {}
+		File dir = new File("C:/dev/test/");
+		
+		while(true) {
+			if(!dir.exists()) {
+				dir.mkdirs();
+			}
+			
+//			if(Thread.currentThread().isDaemon()) {
+				//무한루프 돌리기
+//				System.out.println("데몬이 실행중입니다");
+			System.out.println("프로그램이 실행중입니다.");
+				while(true) {
+					File[] txtList = dir.listFiles(new FilenameFilter() {
+						
+						@Override
+						public boolean accept(File dir, String name) {
+							// TODO Auto-generated method stub
+							boolean isTxt = false;
+							
+							if(name.toLowerCase().startsWith("새") && name.toLowerCase().endsWith(".txt") == true) isTxt = true;
+							
+							return isTxt;
+						}
+					});
+					
+					if(txtList.length > 0) {
+//						threadSleep(600);
+						try {
+							for(File txt : txtList) {
+								//파일이 존재합니다.
+								System.out.println("파일이 존재합니다.");
+								threadSleep(400);
+								deleteTxtFile(txt);
+								System.out.println("파일을 삭제했습니다.");
+								threadSleep(400);
+							}
+						} catch(Exception e) {
+//							threadSleep(400);
+							return;
+						}
+					}
+					
+					threadSleep(800);
+				}
+//			} else {
+//				return;
+//			}
+		}
 
 	}
 
